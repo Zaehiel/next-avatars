fcac57717ddc4c96d917fdfac1713efb118f2c20
Elar Huik <40868085+elarhuik@users.noreply.github.com>
2021-10-25 02:07:54 +0300

    update avatar component

--- lib/models/avatar.ts
+++ lib/models/avatar.ts
@@ -1,5 +1,9 @@
 import { ObjectId } from 'mongodb';
 
 export default class Avatar {
-  constructor(public name: string, public image: string, public id?: ObjectId) {}
+  constructor(
+    public name: string,
+    public image: string,
+    public id?: ObjectId
+  ) {}
 }
