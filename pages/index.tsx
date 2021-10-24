7510efc972780e2ccf7b73760ddae3a3ee5ff341
Elar Huik <40868085+elarhuik@users.noreply.github.com>
2021-10-25 02:09:23 +0300

    update home page to show all uploaded avatars

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Avatar from '../components/Avatar'
+import { base64Prefix } from '../components/Avatar/constants'
+import AvatarModel from '../lib/models/avatar'
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ avatars }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,61 +14,28 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div className={styles.grid}>
+        {
+          avatars.map((a: AvatarModel) => (<Avatar key={a._id} image={base64Prefix + a.image} name={a.name} />))
+        }
+      </div>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const isDev = process.env.NODE_ENV !== 'production';
+  let { DEV_URL, PROD_URL } = process.env;
+  const response = await fetch(`${isDev ? DEV_URL : PROD_URL}/api/avatars`, {
+      method: 'GET',
+  });
+  let data = await response.json();
+
+  return {
+    props: {
+        avatars: data.message,
+    },
+  };
 }
 
 export default Home
